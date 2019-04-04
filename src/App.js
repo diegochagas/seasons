b9ae1f0163c4fbb6a7f39112510511ee9bfb4dc7
Diego Rocha Chagas <drochachagas@gmail.com>
2019-04-04 17:00:43 -0300

    Create state to store the latitude and errorMessage  and include mockup

--- src/App.js
+++ src/App.js
@@ -2,12 +2,31 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        
-      </div>
+  constructor(props){
+    super(props);
+
+    this.state = {
+      latitude: null,
+      errorMessage: ''
+    }
+
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({latitude: position.coords.latitude})
+      }, error => {
+        this.setState({errorMessage: error.message})
+      }
     );
+  }
+
+  render() {
+    if (this.state.latitude && !this.state.errorMessage) {
+      return <div>Latitude: {this.state.latitude}</div>;
+    }
+    if (!this.state.latitude && this.state.errorMessage) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+    return <progress></progress>;
   }
 }
 
